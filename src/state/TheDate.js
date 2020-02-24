cacae44958b78c339ebae441880f6e023d76ce73
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-02-23 21:13:07 -0500

    Adding new project

---
+++ src/state/TheDate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/*class TheDate extends React.Component {
+  render() {
+    return <div />
+  }
+}*/
+
+class TheDate extends React.Component {
+  constructor() {
+
+  }
+  render() {
+    return <div />
+  }
+}
+
+export default TheDate;
