2710fc217f5053d68603340df0487e8d1e40182d
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-07-14 20:29:41 -0400

    Implemented controlled form from Organizing React section of Thinkful materials

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(<App />, document.getElementById('root'));*/
 
 
 //Use the following for the React Router assignment:
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
@@ -22,4 +22,23 @@ ReactDOM.render(
      <App />
    </BrowserRouter>,
   document.getElementById('root')
-);
+);*/
+
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppLang from './lang-context/AppLang';
+
+ReactDOM.render(
+  <AppLang />,
+  document.getElementById('root')
+);*/
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import RegistrationForm from './registrationForm/registrationForm';
+
+ReactDOM.render(<RegistrationForm />, document.getElementById('root'));
